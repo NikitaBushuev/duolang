7f59aee11a8af5f69eb348c17c1f7de7677342a6
NikitaBushuev <nikitabushuev.personal@gmail.com>
2023-09-28 23:47:23 +0600

    Add "includes\duo.h"

---
+++ includes/duo.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <duo/type.h>
+#include <duo/byte.h>
+
+#include <duo/fun.h>
+#include <duo/var.h>
+#include <duo/str.h>
+
+#include <duo/opcode.h>
+#include <duo/operator.h>
